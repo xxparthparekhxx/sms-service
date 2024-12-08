fd6e18090ab537b366fab0ead00293a4cb7f5057
xxparthparekhxx <xx.parthparekh.xx@gmail.com>
2024-12-08 18:14:31 +0530

    added models to the admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Device, Message
+
 
 # Register your models here.
+admin.site.register(Device)
+admin.site.register(Message)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from uuid import uuid4
+
+class Device(models.Model):
+    name= models.CharField(max_length=100)
+    fcm_token = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=15)
+    secret_key = models.CharField(max_length=100, default=uuid4)
+    sms_limit = models.IntegerField(default=0)
+    def __str__(self):
+        return self.name
+    
+
+class Message(models.Model):
+    message = models.CharField(max_length=200)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
